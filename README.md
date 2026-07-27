2b6d13368b70e253ad7e5b58c66b617574b6e112
Magda Chikhradze <magda292005@gmail.com>
2026-07-27 18:48:20 +0400

    feat: create styled form with input validation

--- README.md
+++ README.md
@@ -16,4 +16,9 @@ The React Compiler is not enabled on this template because of its impact on dev 
 If you are developing a production application, we recommend using TypeScript with type-aware lint rules enabled. Check out the [TS template](https://github.com/vitejs/vite/tree/main/packages/create-vite/template-react-ts) for information on how to integrate TypeScript and [`typescript-eslint`](https://typescript-eslint.io) in your project.
 
 ## react hook form
+
 https://react-hook-form.com/
+
+## figma link:
+
+https://www.figma.com/design/dv0figuVofTavfT7gppBw7/payapi-multi-page-website?node-id=0-1931&t=1yab5DX1wmGHmrOD-0
--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -16,65 +16,122 @@ function Form() {
 
   return (
     <>
-      <form onSubmit={handleSubmit(submit)}>
-        <div>
-          <label htmlFor="name">enter your name</label>
-          <input
-            type="text"
-            id="name"
-            className="border"
-            {...register("username", {
-              required: "name is empty, please enter your name",
-              minLength: {
-                value: 2,
-                message: "name must be min 2 characters",
-              },
-            })}
-          />
-          {errors.username && (
-            <p className="text-[red]">{errors.username.message}</p>
-          )}
-        </div>
-        <div>
-          <label htmlFor="email">enter your email</label>
-          <input
-            type="email"
-            id="email"
-            className="border"
-            {...register("email", {
-              required: "Email is required",
-              pattern: {
-                value: /^[^\s@]+@[^\s@]+\.[^\s@]+$/,
-                message: "Please enter a valid email address",
-              },
-            })}
-          />
-          {errors.email && <p className="text-[red]">{errors.email.message}</p>}
-        </div>
-        <div>
-          <label htmlFor="password">enter your password</label>
-          <input
-            type="password"
-            id="password"
-            className="border"
-            {...register("password", {
-              required: "password is empty",
-              minLength: {
-                value: 8,
-                message: "password is too short",
-              },
-              pattern: {
-                value: /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[\W_]).{8,}$/,
-                message: "make stronger password",
-              },
-            })}
-          />
-          {errors.password && (
-            <span className="text-[red]">{errors.password.message}</span>
-          )}
-        </div>
-        <button className="border">submit</button>
-      </form>
+      <main className="flex flex-col items-center bg-[#EDF3F8] min-h-screen">
+        <h1 className="mt-18.75 mb-13.5 text-[48px] w-143.25 tracking-[-0.37px] leading-14 text-center text-[#36536B] font-normal">
+          Submit a help request and we’ll get in touch shortly.
+        </h1>
+        <form onSubmit={handleSubmit(submit)} className="w-111.25 h-124.75 ">
+          <div className="flex flex-col ">
+            <label htmlFor="name" className="text-[15px]">
+              enter your name
+            </label>
+            <input
+              type="text"
+              id="name"
+              className="border-b focus:outline-none pl-2"
+              {...register("username", {
+                required: "name is empty, please enter your name",
+                minLength: {
+                  value: 2,
+                  message: "name must be min 2 characters",
+                },
+              })}
+            />
+            {errors.username && (
+              <p className="text-[red] text-[12px]">
+                {errors.username.message}
+              </p>
+            )}
+          </div>
+          <div className="flex flex-col ">
+            <label htmlFor="email" className="text-[15px]">
+              enter your email
+            </label>
+            <input
+              type="email"
+              id="email"
+              className="border-b focus:outline-none pl-2"
+              {...register("email", {
+                required: "Email is required",
+                pattern: {
+                  value: /^[^\s@]+@[^\s@]+\.[^\s@]+$/,
+                  message: "Please enter a valid email address",
+                },
+              })}
+            />
+            {errors.email && (
+              <p className="text-[red] text-[12px] ">{errors.email.message}</p>
+            )}
+          </div>
+          <div className="flex flex-col ">
+            <label htmlFor="company" className="text-[15px]">
+              enter your company name
+            </label>
+            <input
+              type="text"
+              id="company"
+              className="border-b focus:outline-none pl-2"
+              {...register("company", {
+                required: "company name is empty",
+              })}
+            />
+            {errors.company && (
+              <span className="text-[red] text-[12px]">
+                {errors.company.message}
+              </span>
+            )}
+          </div>
+          <div className="flex flex-col  ">
+            <label htmlFor="title" className="text-[15px]">
+              enter the title
+            </label>
+            <input
+              type="text"
+              id="title"
+              className="border-b focus:outline-none pl-2"
+              {...register("title", {
+                required: "title is empty, please enter your title",
+                minLength: {
+                  value: 2,
+                  message: "title must be min 2 characters",
+                },
+              })}
+            />
+            {errors.title && (
+              <p className="text-[red] text-[12px]">{errors.title.message}</p>
+            )}
+          </div>
+          <div className="flex flex-col ">
+            <label htmlFor="message" className="mb-6 text-[15px] ">
+              enter a message
+            </label>
+            <input
+              type="message"
+              id="message"
+              className="border-b focus:outline-none pl-2"
+              {...register("message", {
+                required: "message is empty, please enter your title",
+                minLength: {
+                  value: 18,
+                  message: "message must be min 18 characters",
+                },
+              })}
+            />
+            {errors.message && (
+              <p className="text-[red] text-[12px]">{errors.message.message}</p>
+            )}
+          </div>
+          <div className="mt-6">
+            <input type="checkbox" id="checkbox" />
+            <label htmlFor="checkbox">
+              Stay up-to-date with company announcements and updates to our API
+            </label>
+          </div>
+          <button className="w-38 h-12 bg-transparent border border-[#36536B] rounded-3xl mt-6">
+            submit
+          </button>
+        </form>
+      </main>
     </>
   );
 }
