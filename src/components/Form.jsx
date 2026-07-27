f15cad91868acd866b4cab3521bda4727fdee297
Magda Chikhradze <magda292005@gmail.com>
2026-07-27 17:07:12 +0400

    feat: add basic form with react-hook-form

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -5,20 +5,40 @@ function Form() {
   const { register, handleSubmit } = useForm();
   console.log(register);
 
+  const submit = (data) => {
+    console.log(data);
+    console.log(data.username);
+  };
+
   return (
     <>
-      <form>
+      <form onSubmit={handleSubmit(submit)}>
         <div>
           <label htmlFor="name">enter your name</label>
-          <input type="text" id="name" className="border" />
+          <input
+            type="text"
+            id="name"
+            className="border"
+            {...register("username")}
+          />
         </div>
         <div>
           <label htmlFor="email">enter your email</label>
-          <input type="email" id="email" className="border" />
+          <input
+            type="email"
+            id="email"
+            className="border"
+            {...register("email")}
+          />
         </div>
         <div>
           <label htmlFor="password">enter your password</label>
-          <input type="password" id="password" className="border" />
+          <input
+            type="password"
+            id="password"
+            className="border"
+            {...register("password")}
+          />
         </div>
         <button className="border">submit</button>
       </form>
