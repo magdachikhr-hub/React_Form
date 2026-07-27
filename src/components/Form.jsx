8b4cc0535019cfbd90074e0268688e229637ee59
Magda Chikhradze <magda292005@gmail.com>
2026-07-27 17:49:25 +0400

    feat: add error messages

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -2,7 +2,11 @@ import React from "react";
 import { useForm } from "react-hook-form";
 
 function Form() {
-  const { register, handleSubmit } = useForm();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
   console.log(register);
 
   const submit = (data) => {
@@ -19,8 +23,17 @@ function Form() {
             type="text"
             id="name"
             className="border"
-            {...register("username")}
+            {...register("username", {
+              required: "name is empty, please enter your name",
+              minLength: {
+                value: 2,
+                message: "name must be min 2 characters",
+              },
+            })}
           />
+          {errors.username && (
+            <p className="text-[red]">{errors.username.message}</p>
+          )}
         </div>
         <div>
           <label htmlFor="email">enter your email</label>
@@ -28,8 +41,15 @@ function Form() {
             type="email"
             id="email"
             className="border"
-            {...register("email")}
+            {...register("email", {
+              required: "Email is required",
+              pattern: {
+                value: /^[^\s@]+@[^\s@]+\.[^\s@]+$/,
+                message: "Please enter a valid email address",
+              },
+            })}
           />
+          {errors.email && <p className="text-[red]">{errors.email.message}</p>}
         </div>
         <div>
           <label htmlFor="password">enter your password</label>
@@ -37,8 +57,21 @@ function Form() {
             type="password"
             id="password"
             className="border"
-            {...register("password")}
+            {...register("password", {
+              required: "password is empty",
+              minLength: {
+                value: 8,
+                message: "password is too short",
+              },
+              pattern: {
+                value: /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[\W_]).{8,}$/,
+                message: "make stronger password",
+              },
+            })}
           />
+          {errors.password && (
+            <span className="text-[red]">{errors.password.message}</span>
+          )}
         </div>
         <button className="border">submit</button>
       </form>
